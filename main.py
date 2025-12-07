cc68ad8d904e83a87658c9533624ca486cf40b67
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-07 14:16:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
 POST_ID = 304394  # 🔧 Marina Protocol
-TARGET_H2_TEXT = "Marina Protocol Today Quiz Answer for December 6, 2025"
-CHECK_ANSWER = "B) Real estate."
+TARGET_H2_TEXT = "Marina Protocol Today Quiz Answer for December 7, 2025"
+CHECK_ANSWER = "B) Legal compliance and real-world linkage."
 
 # Find & Replace ngày
-OLD_DATE = "December 6"
-NEW_DATE = "December 7"
+OLD_DATE = "December 7"
+NEW_DATE = "December 8"
 
 
 # ================ SCRAPE SITE ================
