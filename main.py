a2e3c5c12884ed43a0881a0e6fd2ae0a0e308c54
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-01-07 15:36:12 +0700

    Update POST_ID and CHECK_ANSWER values

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 312604  # 🔧 Cập nhật đúng ID bài Marina Protocol
+POST_ID = 312840  # 🔧 Cập nhật đúng ID bài Marina Protocol
 TARGET_H2_TEXT = "Marina Protocol Today Quiz Answer for December 19, 2025"
-CHECK_ANSWER = "A) Token rewards for engagement."
+CHECK_ANSWER = "A) Network effects."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
