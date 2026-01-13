210fd91467f89c2397a4a25c16c24277fdc4b39d
xialing267-droid <xialing267@gmail.com>
2026-01-13 16:25:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 314092  # 🔧 Cập nhật đúng ID bài Marina Protocol
+POST_ID = 314404  # 🔧 Cập nhật đúng ID bài Marina Protocol
 TARGET_H2_TEXT = "Marina Protocol Today Quiz Answer for December 19, 2025"
-CHECK_ANSWER = "C) Bitcoin."
+CHECK_ANSWER = "A) Decentralized Finance."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
