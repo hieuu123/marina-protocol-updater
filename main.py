5d2df31775bfc0ad8359fad73ddfc65d1efc2ef2
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-03-08 16:55:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 322368  # 🔧 Cập nhật đúng ID bài Marina Protocol
+POST_ID = 322393  # 🔧 Cập nhật đúng ID bài Marina Protocol
 TARGET_H2_TEXT = "Marina Protocol Today Quiz Answer for December 19, 2025"
-CHECK_ANSWER = "A) An agent that can execute smart contracts autonomously."
+CHECK_ANSWER = "B) Enhanced automation and trustless execution."
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
     url = "https://miningcombo.com/marina-protocol"
